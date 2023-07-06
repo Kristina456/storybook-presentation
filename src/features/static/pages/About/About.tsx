92a3c97fc49a359d05f0355618fea1f2999f473b
Kristina Persic <persic.kristina@gmail.com>
2023-07-06 14:25:24 +0200

    feat: add About page

--- src/features/static/pages/About/About.tsx
+++ src/features/static/pages/About/About.tsx
@@ -1,8 +1,35 @@
-export const About: React.FC = () => {
+import React from 'react';
+import { Footer, Header } from '../../components';
+import styles from './About.module.scss';
+
+type User = {
+  name: string;
+};
+
+export const About = () => {
+  const [user, setUser] = React.useState<User>();
 
   return (
-    <div className="home">
-     About page
+    <div className={styles['about']}>
+      <Header
+        user={user}
+        onLogin={() => setUser({ name: 'Jane Doe' })}
+        onLogout={() => setUser(undefined)}
+        onCreateAccount={() => setUser({ name: 'Jane Doe' })}
+      />
+      <section className={styles['about__section']}>
+        <h2 className={styles['about__section-title']}>About page</h2>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ad nisi
+          delectus assumenda quisquam, nihil voluptas quam deserunt repudiandae
+          explicabo quis et eum dicta necessitatibus voluptate optio velit
+          consectetur pariatur! Voluptate, suscipit beatae repellendus explicabo
+          eum, tempore qui ea, eos numquam sit quasi sint quae eaque adipisci
+        </div>
+      </section>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
